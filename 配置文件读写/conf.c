382cbbc1c10a26bb2e89ba9c14104498a1000c9a
hn2zzzz1996 <huanggua5678@qq.com>
2019-12-18 20:01:50 +0800

    conf

--- 配置文件读写/conf.c
+++ 配置文件读写/conf.c
@@ -36,21 +36,27 @@ conf_t *new_conf() {
 
 // 去除前后的空格
 char *trim_space(char *str) {
+    char *tmp = Malloc(strlen(str) + 1);
+    memcpy(tmp, str, strlen(str) + 1);
+    str = tmp;
+
     int len = strlen(str);
     char *head = str, *tail = str + len - 1;
     while(*head == ' ' || *head == '\t') {
         head++;
     }
     if(head - str == len) {
+        printf("NULL\n");
         return NULL;
     }
 
-    while(*tail == ' ' || *tail == '\t') {
+    while(*tail == ' ' || *tail == '\t' || *tail == '\n') {
         tail--;
     }
     *(tail+1) = '\0';
 
-    return head;
+    memcpy(str, head, tail-head+1);
+    return str;
 }
 
 entry_t *find_entry(conf_t *conf, char *key) {
@@ -70,27 +76,38 @@ void new_entry(conf_t *conf, char *key, char *value) {
     entry->key = Malloc(strlen(key)+1);
     entry->value = Malloc(strlen(value)+1);
     memcpy(entry->key, key, strlen(key)+1);
-    memcpy(entry->value, value, sizeof(value)+1);
+    memcpy(entry->value, value, strlen(value)+1);
     
     entry->next = conf->entry_head;
     conf->entry_head = entry;
 }
 
 int set_conf(conf_t *conf, char *key, char *value) {
+    int ret = 0;
     key = trim_space(key);
     value = trim_space(value);
     if(key && value) {
-        printf("%s = %sx\n", key, value);
+        printf("%s = %s\n", key, value);
     } else {
-        fprintf(stderr, "key or value is null\n");
+        fprintf(stderr, "Error: key or value is null\n");
+        ret = -1;
+        goto Free;
     }
 
     entry_t *entry = find_entry(conf, key);
     if(entry == NULL) {
+        //printf("new_entry\n");
         new_entry(conf, key, value);
     } else {
+        // update it
         entry->value = value;
     }
+
+Free:
+    free(key); 
+    free(value);
+
+    return ret;
 }
 
 char* get_conf(conf_t *conf, char *key) {
@@ -99,6 +116,36 @@ char* get_conf(conf_t *conf, char *key) {
         return entry->value;
     } 
     return NULL;
+}
+
+void free_entry(entry_t *entry) {
+    free(entry->key);
+    free(entry->value);
+    free(entry);
+}
+
+int del_conf(conf_t *conf, char *key) {
+    entry_t *entry = conf->entry_head, *prev = NULL;
+    while(entry) {
+        if(strcmp(entry->key, key) == 0) {
+            break;
+        }
+        prev = entry;
+        entry = entry->next;
+    }
+    if(entry) {
+        if(prev == NULL) {
+            conf->entry_head = conf->entry_head->next;
+        } else {
+            prev->next = entry->next;
+            free_entry(entry);
+        }
+    } else {
+        printf("key is not exist!\n");
+        return -1;
+    }
+
+    return 0;
 }
 
 typedef enum STATE {
@@ -124,6 +171,10 @@ conf_t* open_conf_file(const char *filename) {
         STATE state = START;
 
         for(tmp = line; *tmp; tmp++) {
+            if(*tmp == '#') {
+                *tmp = '\0';
+                break;
+            }
             if(state == START) {
                 key = tmp;
                 state = KEY;
@@ -132,19 +183,12 @@ conf_t* open_conf_file(const char *filename) {
                 if(*tmp == '=') {
                     *tmp = '\0';
                     state = EQUAL;
-                } else if(*tmp == '#') {
-                    *tmp = '\0';
-                    break;
-                }
+                } 
             } else if(state == EQUAL) {
-                if(*tmp == '#') break;
                 value = tmp;
                 state = VALUE;
             } else if(state == VALUE) {
-                if(*tmp == '#') {
-                    *tmp = '\0';
-                    break;
-                }
+                // when it reach the '\n' or '#', break
             }
         }
 
@@ -170,9 +214,11 @@ void save_conf_file(conf_t *conf, const char *name) {
 
 int main() {
     conf_t *conf = open_conf_file("test.conf");
-    //printf("get_conf: \"%s\"\n", get_conf(conf, "hsq"));
     printf("get_conf: \"%s\"\n", get_conf(conf, "a a"));
-    //printf("get_conf: \"%s\"\n", get_conf(conf, "aaaa"));
-    //printf("get_conf: \"%s\"\n", get_conf(conf, "bbbb"));
-    //save_conf_file(conf, "abc.conf");
+    printf("get_conf: \"%s\"\n", get_conf(conf, "tommorow this is a    good"));
+    //del_conf(conf, "hsq");
+    //set_conf(conf, "sss", "789 100");
+    save_conf_file(conf, "abc.conf");
+
+    printf("All is ok!\n");
 }
